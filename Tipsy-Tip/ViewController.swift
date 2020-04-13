1958b82046363b908b4c711391bed1f932a40d2f
Mariah Baysic <mjpb123@gmail.com>
2020-04-13 14:27:55 +0800

    initial commit

---
+++ Tipsy-Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipsy-Tip
+//
+//  Created by Mariah Baysic on 4/13/20.
+//  Copyright © 2020 SpacedOut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
